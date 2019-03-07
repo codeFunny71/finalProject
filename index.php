647a191a4e663d190c85c0421d691becab487a40
Mike Prelesnik <mprelesnik@mail.greenriver.edu>
2019-03-07 15:22:12 -0500

    set up routing between the three current pages minus backgrounds and css not showing up

--- index.php
+++ index.php
@@ -1,64 +1,42 @@
 <?php
 
+//turn on error reporting
+//ini_set('display_errors', 1);
+//error_reporting(E_ALL);
+
 //Required files
 require_once 'vendor/autoload.php';
-require_once 'model/db_functions.php';
 //Start session AFTER autoload
 session_start();
 //Create an instance of the Base class
 $f3 = Base::instance();
-//Debugging
-//require_once '/home/mabsherg/public_html/debug.php';
-//Connect to the database
-$dbh = connect();
+
+//turn on fat-free error reporting
+$f3->set('DEBUG', 3);
+
+
+
 //Define a default route
-$f3->route('GET /', function($f3) {
-    $students = getStudents();
-    $f3->set('students', $students);
+$f3->route('GET|POST /', function() {
     //load a template
     $template = new Template();
-    echo $template->render('views/all-students.html');
+    echo $template->render('views/home.html');
 });
-//Define a route to view a student summary
-$f3->route('GET /summary', function() {
+
+//sign up route
+$f3->route('GET|POST /signup', function() {
     //load a template
     $template = new Template();
-    echo $template->render('views/view-student.html');
+    echo $template->render('views/signUp.html');
 });
-//Define a route to add a student
-$f3->route('GET|POST /add', function($f3) {
-    //print_r($_POST);
-    /*
-     * Array (  [sid] => 5678
-     *          [last] => Shin
-     *          [first] => Jen
-     *          [birthdate] => 2000-08-08
-     *          [gpa] => 4.0
-     *          [advisor] => 1
-     *          [submit] => Submit )
-     */
-    if(isset($_POST['submit'])) {
-        //Get the form data
-        $sid = $_POST['sid'];
-        $last = $_POST['last'];
-        $first = $_POST['first'];
-        $birthdate = $_POST['birthdate'];
-        $gpa = $_POST['gpa'];
-        $advisor = $_POST['advisor'];
-        //Validate the data
-        //Add the student
-        $success = addStudent($sid, $last, $first, $birthdate,
-            $gpa, $advisor);
-        if($success) {
-            $student = new Student($sid, $last, $first, $birthdate,
-                $gpa, $advisor);
-            $_SESSION['student'] = $student;
-            $f3->reroute('/summary');
-        }
-    }
+
+//logged in homepage route
+$f3->route('GET|POST /homepage', function() {
     //load a template
     $template = new Template();
-    echo $template->render('views/add-student.html');
+    echo $template->render('views/loggedInHP.html');
 });
+
+
 //Run fat free
 $f3->run();
